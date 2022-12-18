966b25b01e295a67eca5b78339b156a97a5d5d21
zhicheng <hi@unickcheng.cc>
2022-12-18 21:46:40 +0800

    fix: update aghome dockerfile

--- AdGuardHome-and-Clash/config/AdGuardHome.sh
+++ AdGuardHome-and-Clash/config/AdGuardHome.sh
@@ -3,6 +3,6 @@ sysctl -p
 iptables -t filter -A FORWARD -j ACCEPT
 iptables -t nat -A POSTROUTING -s ${SUBNET} -j MASQUERADE
 
-sed -i "s/bind_port:.*/bind_port: ${ WEB_PORT }" /opt/AdGuardHome/conf/AdGuardHome.yaml
+sed -i "s/bind_port:.*/bind_port: ${ WEB_PORT }" /AdGuardHome/conf/AdGuardHome.yaml
 
-/opt/AdGuardHome/AdGuardHome -c /opt/AdGuardHome/conf/AdGuardHome.yaml -h 0.0.0.0 -w /opt/AdGuardHome/work
+/AdGuardHome/AdGuardHome -c /AdGuardHome/conf/AdGuardHome.yaml -h 0.0.0.0 -w /AdGuardHome/work
