65b79b4c53735966acd5d29d6b662e0e84f10ebe
zhicheng <hi@unickcheng.cc>
2022-12-18 23:20:11 +0800

    fix: variable is empty judgment

--- AdGuardHome-and-Clash/config/AdGuardHome.sh
+++ AdGuardHome-and-Clash/config/AdGuardHome.sh
@@ -1,8 +1,13 @@
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
-iptables -t filter -A FORWARD -j ACCEPT
-iptables -t nat -A POSTROUTING -s ${SUBNET} -j MASQUERADE
 
-sed -i "s/bind_port:.*/bind_port: ${ WEB_PORT }" /AdGuardHome/conf/AdGuardHome.yaml
+if [ ! ${SUBNET} ]; then
+    iptables -t filter -A FORWARD -j ACCEPT
+    iptables -t nat -A POSTROUTING -s ${SUBNET} -j MASQUERADE
+fi
+
+if [ ! ${WEB_PORT} ]; then
+    sed -i "s/bind_port:.*/bind_port: ${WEB_PORT}" /AdGuardHome/conf/AdGuardHome.yaml
+fi
 
 /AdGuardHome/AdGuardHome -c /AdGuardHome/conf/AdGuardHome.yaml -h 0.0.0.0 -w /AdGuardHome/work
